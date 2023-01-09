5d1d8b0d12653fc883b004d264f0e9f353636ddc
suzhanye <2460131984@qq.com>
2023-01-09 09:53:17 +0800

    lesson绝对在线课堂

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
+// import Welcome from '@/views/welcome'
 
 // 所有组件的路由都在这个数组中配置
 const routes = [
-  {path: '/banner', name: 'banner', component: import('@/views/welcome/banner')},
-  {path: '/video-list', name: 'video-list', component: import('@/views/welcome/video-list')}
+  // {path: '/', name: 'Welcome', component: Welcome},
+  {path: '/banner', name: 'Banner', component: import('@/views/welcome/banner')},
+  {path: '/video-list', name: 'VideoList', component: import('@/views/welcome/video-list')},
+  {path: '/common-footer', name: 'CommonFooter', component: import('@/components/common-footer')}
 ]
 
 const router = createRouter({
