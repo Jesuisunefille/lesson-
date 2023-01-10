e036d076feb33c118db7da45b4104f86f25e553c
suzhanye <2460131984@qq.com>
2023-01-10 09:28:08 +0800

    lesson绝对在线课堂

--- lesson-user-background/src/main/java/com/cindy/UserBackgroundApp.java
+++ lesson-user-background/src/main/java/com/cindy/UserBackgroundApp.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @author Cindy
+ */
 @EnableScheduling
 @EnableAsync
 @MapperScan("com.cindy.mapper")
