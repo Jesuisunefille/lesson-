b776d0d57fe6080218cbb4a41e2a517acdcf15fc
suzhanye <2460131984@qq.com>
2023-01-14 20:58:22 +0800

    lesson绝对在线课堂

--- lesson-user-background/src/main/java/com/cindy/UserBackgroundApp.java
+++ lesson-user-background/src/main/java/com/cindy/UserBackgroundApp.java
@@ -12,9 +12,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 @MapperScan("com.cindy.mapper")
-@SpringBootApplication
+@SpringBootApplication     //启动类注解，用于自动装配类和扫描所在包及子包的@Component注解
 public class UserBackgroundApp {
     public static void main(String[] args) {
+        //启动代码
         SpringApplication.run(UserBackgroundApp.class, args);
     }
 }
